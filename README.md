c708ec212cadb3d7751b4f7ce04fe09f9eb86855
roman <rb@synac.com>
2017-10-04 17:26:34 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+This is the TYPO3 Introduction Package
+======================================
+
+
+This distribution is used to create web sites based on TYPO3 version 8 and
+the bootstrap package from Benjamin Kott.
+
+
+Credits
+-------
+
+Kudos go to all contributors:
+
+* Benjamin Kott for the large part of developing this extension,
+* Sven Wolfermann for the first version of the package,
+* Ben van't Ende and Mathias Schreiber for kicking butts.
+
+
+
+Links
+-----
+
+Box creator: https://www.insofta.com/cover-commander/3d-cover-generator/
+
---
+++ ext_emconf.php
@@ -0,0 +1,25 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "pizpalue".
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Piz Palü Distribution',
+	'description' => 'This distribution is based on TYPO3 version 8 and the bootstrap package from Benjamin Kott.',
+	'category' => 'distribution',
+	'version' => '8.0.1',
+	'state' => 'stable',
+	'clearcacheonload' => 1,
+	'author' => 'Roman Büchler',
+	'author_email' => 'rb@synac.com',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '8.0.0-8.99.99',
+			'bootstrap_package' => '8.0.0-8.99.99',
+            'bpsynplate' => '8.0.0-8.99.99'
+		),
+		'conflicts' => array(),
+		'suggests' => array(),
+	)
+);
