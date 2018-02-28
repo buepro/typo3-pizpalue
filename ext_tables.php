e90936e03368fcb25df1f92651ea60aedd7d6111
Roman <rb@synac.com>
2018-02-28 11:53:05 +0100

    add default theme through static ts

--- ext_tables.php
+++ ext_tables.php
@@ -26,21 +26,23 @@ if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['bootstrap_package'])
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,
     'Configuration/TypoScript/Main', 'Pizpalue - Main');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,
+    'Configuration/TypoScript/Theme', 'Pizpalue - Theme');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,
     'Configuration/TypoScript/Grids', 'Pizpalue - Grid CEs');
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,
-        'Extensions/t3s_jslidernews/Configuration/TypoScript', 'Pizpalue - t3s_jslidernews 5.0.2');
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,
-        'Extensions/powermail/2.17.2/Configuration/TypoScript', 'Pizpalue - powermail 2.17.2');
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,
-        'Extensions/powermail/3.7.0/Configuration/TypoScript', 'Pizpalue - powermail 3.7.0');
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Extensions/news/Configuration/TypoScript',
-        'Pizpalue - news 6.0.0');
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,
-        'Extensions/newsslider/Configuration/TypoScript', 'Pizpalue - newsslider 2.0.1');
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,
-        'Extensions/femanager/Configuration/TypoScript', 'Pizpalue - femanager 2.2.0');
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,
-        'Extensions/tt_address/Configuration/TypoScript/3.0.3', 'Pizpalue - tt_address 3.0.3');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,
+    'Extensions/t3s_jslidernews/Configuration/TypoScript', 'Pizpalue - t3s_jslidernews 5.0.2');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,
+    'Extensions/powermail/2.17.2/Configuration/TypoScript', 'Pizpalue - powermail 2.17.2');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,
+    'Extensions/powermail/3.7.0/Configuration/TypoScript', 'Pizpalue - powermail 3.7.0');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Extensions/news/Configuration/TypoScript',
+    'Pizpalue - news 6.0.0');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,
+    'Extensions/newsslider/Configuration/TypoScript', 'Pizpalue - newsslider 2.0.1');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,
+    'Extensions/femanager/Configuration/TypoScript', 'Pizpalue - femanager 2.2.0');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,
+    'Extensions/tt_address/Configuration/TypoScript/3.0.3', 'Pizpalue - tt_address 3.0.3');
 
 
 /***************
