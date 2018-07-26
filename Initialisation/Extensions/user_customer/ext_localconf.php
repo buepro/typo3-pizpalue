98c78ac4c16f4e7aa114a0f8992c55cc82a39d78
Roman <rb@buechler.pro>
2018-07-26 12:06:17 +0200

    [FEATURE] Add customization extension user_customer

---
+++ Initialisation/Extensions/user_customer/ext_localconf.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+ * This file is part of the package buepro/user_customer.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
---
+++ Initialisation/Extensions/user_customer/ext_tables.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file is part of the package buepro/user_customer.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Customer');
