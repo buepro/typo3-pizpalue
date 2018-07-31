ad5aee461b719ae8bfd7303296e1e59b43c20583
Roman <rb@buechler.pro>
2018-07-31 10:32:45 +0200

    [FEATURE] Add background image for content element

--- ext_tables.sql
+++ ext_tables.sql
@@ -7,6 +7,7 @@ CREATE TABLE tt_content (
 	tx_pizpalue_classes varchar(255) DEFAULT '' NOT NULL,
 	tx_pizpalue_style varchar(255) DEFAULT '' NOT NULL,
 	tx_pizpalue_attributes varchar(255) DEFAULT '' NOT NULL,
+	tx_pizpalue_bgmedia int(11) unsigned NOT NULL default '0',
 
 
 );
