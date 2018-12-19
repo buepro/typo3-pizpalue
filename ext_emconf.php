615b3b43df58ac2e16f64c1688bac5ec60e5f86e
Roman <rb@buechler.pro>
2018-12-19 19:40:20 +0100

    [TASK] Bootstrap less embedding

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = [
             'ws_flexslider'         => '1.5.6-1.5.99',
             'indexed_search'        => '8.7.17-9.99.99',
             //'brt_videourlreplace'   => '1.2.1-1.2.99',
-            'user_customer'         => '9.0.0-9.99.99',
+            'user_customer'         => '8.0.0-9.99.99',
         ],
         'conflicts' => [],
         'suggests'  => [],
