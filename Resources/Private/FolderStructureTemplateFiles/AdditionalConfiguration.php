af7a02a2641433c943d80079fd88e5cf95a593b4
Roman <rb@buechler.pro>
2018-12-20 13:16:09 +0100

    [TASK] Add configuration to template file

--- Resources/Private/FolderStructureTemplateFiles/AdditionalConfiguration.php
+++ Resources/Private/FolderStructureTemplateFiles/AdditionalConfiguration.php
@@ -1,3 +1,6 @@
 <?php
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['ddmmyy'] = 'd.m.Y';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['hhmm'] = 'H:i';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['phpTimeZone'] = 'Europe/Zurich';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['systemLocale'] = 'de_CH.utf8';
 $GLOBALS['TYPO3_CONF_VARS']['BE']['lockSSL'] = true;
