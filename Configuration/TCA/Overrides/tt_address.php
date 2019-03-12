a755ff16c78f1c10f90e22d55f5b6c2af4317850
Roman <rb@buechler.pro>
2019-03-12 20:50:43 +0100

    [TASK] Addressteaser: Adds scaff with flip cards

--- Configuration/TCA/Overrides/tt_address.php
+++ Configuration/TCA/Overrides/tt_address.php
@@ -4,3 +4,8 @@
     'Extensions/tt_address/GoogleMap/Configuration/TsConfig/Page.tsconfig',
     'Pizpalue - Extension tt_address Google map'
 );
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    'pizpalue',
+    'Extensions/tt_address/Teaser/Configuration/TsConfig/Page.tsconfig',
+    'Pizpalue - Extension tt_address Teaser'
+);
