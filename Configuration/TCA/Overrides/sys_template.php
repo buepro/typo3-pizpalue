db4772c2391e5b6e014d27a4fc7d13b778aa4694
Roman <rb@buechler.pro>
2019-03-18 18:57:18 +0100

    [FEATURE] Adds package for extension indexed_search

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -74,6 +74,11 @@ call_user_func(
         );
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
             $_EXTKEY,
+            'Extensions/indexed_search/Configuration/TypoScript',
+            'Pizpalue - indexed_search'
+        );
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $_EXTKEY,
             'Extensions/tt_address/3.0.3/Configuration/TypoScript',
             'Pizpalue DEPRECIATED - tt_address 3.0.3'
         );
