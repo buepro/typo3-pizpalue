5a6cca3e9213484b794a48cc2ca3fa8a971ecb0c
Roman <rb@buechler.pro>
2019-04-18 12:27:24 +0300

    [TASK] Completes new content element ModalDialog

--- Configuration/TCA/Overrides/200_content_element_modal_dialog.php
+++ Configuration/TCA/Overrides/200_content_element_modal_dialog.php
@@ -4,14 +4,14 @@ defined('TYPO3_MODE') || die();
 
 call_user_func(function ($extensionKey) {
 
-    /***************
-     * Enable Content Element
+    /**
+     * Enables Content Element
      */
     if (!is_array($GLOBALS['TCA']['tt_content']['types']['modal_dialog'])) {
         $GLOBALS['TCA']['tt_content']['types']['modal_dialog'] = [];
     }
 
-    /***************
+    /**
      * Adds content element to new content element wizard
      */
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
@@ -20,7 +20,7 @@ call_user_func(function ($extensionKey) {
         'Pizpalue Content Element: Modal Dialog'
     );
 
-    /***************
+    /**
      * Adds content element to selector list
      */
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
@@ -31,6 +31,50 @@ call_user_func(function ($extensionKey) {
             'modal_dialog',
             'content-pizpalue-modal-dialog'
         ]
+    );
+
+    /**
+     * Assigns Icon to page view
+     */
+    $GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['modal_dialog'] = 'content-pizpalue-modal-dialog';
+
+    /**
+     * Configures element type
+     */
+    $GLOBALS['TCA']['tt_content']['types']['modal_dialog'] = array_replace_recursive(
+        $GLOBALS['TCA']['tt_content']['types']['modal_dialog'],
+        [
+            'showitem' => '
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+                    --palette--;;general,
+                    header;LLL:EXT:pizpalue/Resources/Private/Language/locallang_db.xlf:tca.modal_dialog.header,
+                    records;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:records_formlabel,
+                --div--;LLL:EXT:bootstrap_package/Resources/Private/Language/Backend.xlf:menu.card.options,
+                    pi_flexform;LLL:EXT:pizpalue/Resources/Private/Language/locallang_db.xlf:tca.modal_dialog.options,
+                --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+                    --palette--;;frames,
+                    --palette--;;appearanceLinks,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+                    --palette--;;language,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+                    --palette--;;hidden,
+                    --palette--;;access,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+                    categories,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+                    rowDescription,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+            '
+        ]
+    );
+
+    /**
+     * Adds flexForm for content element configuration
+     */
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+        '*',
+        'FILE:EXT:pizpalue/Configuration/FlexForms/ModalDialog.xml',
+        'modal_dialog'
     );
 
 },'pizpalue');
