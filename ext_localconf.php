e18b39a2fb1b4126e2a7b91bd51b65ea0e8e8037
Roman <rb@buechler.pro>
2019-09-21 14:06:01 +0300

    [TASK] Drop automatic language menu polyfill and BE styling for TYPO3 8

--- ext_localconf.php
+++ ext_localconf.php
@@ -13,41 +13,42 @@
 defined('TYPO3_MODE') or die('Access denied.');
 
 /**
- * Configure BackendLayout
+ * Make bootstrap_package configuration accessible
  */
 if (1) {
-    if (class_exists(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)) {
-        // Add BackendLayouts BackendLayouts for the BackendLayout DataProvider
-        $extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-            \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
-        );
-        $bootstrapPackageConfiguration = $extensionConfiguration->get('bootstrap_package');
-    } else {
-        // Fallback for TYPO3 V8
-        // @extensionScannerIgnoreLine
-        $bootstrapPackageConfiguration = $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['bootstrap_package'];
-        if (!is_array($bootstrapPackageConfiguration)) {
-            $bootstrapPackageConfiguration = unserialize($bootstrapPackageConfiguration);
-        }
-    }
+    $extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+        \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
+    );
+    $bootstrapPackageConfiguration = $extensionConfiguration->get('bootstrap_package');
+}
+
+/**
+ * PageTS
+ */
+if (1) {
+
+    // Add BackendLayouts for the BackendLayout DataProvider
     if (!$bootstrapPackageConfiguration['disablePageTsBackendLayouts']) {
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
             '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:pizpalue/Configuration/TsConfig/Page/Mod/WebLayout/BackendLayouts.tsconfig">'
         );
     }
-}
 
-/**
- * Configure RTE
- */
-if (1) {
+    // RTE
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
         '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:pizpalue/Configuration/TsConfig/Page/RTE.tsconfig">'
     );
-
-    // Add default RTE configuration
-    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['pizpalue'] = 'EXT:pizpalue/Configuration/RTE/Default.yaml';
 }
+
+/**
+ * Add default RTE configuration for pizpalue
+ */
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['pizpalue'] = 'EXT:pizpalue/Configuration/RTE/Default.yaml';
+
+/***************
+ * Register "pp" as global fluid namespace
+ */
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['pp'][] = 'Buepro\\Pizpalue\\ViewHelpers';
 
 /**
  * Backend styling TYPO3 9
@@ -60,38 +61,6 @@ if (TYPO3_MODE === 'BE') {
         \Buepro\Pizpalue\Service\BrandingService::class,
         'setBackendStyling'
     );
-}
-
-/**
- * Backend styling TYPO3 8
- */
-if (TYPO3_MODE === 'BE' && !class_exists(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)) {
-    //Configure Backend Extension
-    if (!is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend'])) {
-        $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend'] = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']);
-    }
-    $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']['loginLogo'] = 'EXT:pizpalue/Resources/Public/Images/backend-login-logo.png';
-    $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']['loginBackgroundImage'] = 'EXT:pizpalue/Resources/Public/Images/backend-login-background.jpg';
-    if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend'])) {
-        $GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend'] = serialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['backend']);
-    }
-}
-
-/**
- * Language menu for TYPO3 8
- */
-if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) < 9000000) {
-    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptConstants(implode(LF, [
-        'config.language.default {',
-        '    enable = 1',
-        '    title = German',
-        '    nav_title = Deutsch',
-        '    locale = de_CH.UTF-8',
-        '    language_isocode = de',
-        '    hreflang = de-CH',
-        '    direction = ltr',
-        '}'
-    ]));
 }
 
 /**
