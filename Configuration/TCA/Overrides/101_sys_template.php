c5adc6fef07f0135b89cad38430e23a30fed6498
roman <rb@buechler.pro>
2021-03-04 11:21:26 +0200

    [TASK] Change list from static templates for extensions
    List static templates only if related extension is installed.

--- Configuration/TCA/Overrides/101_sys_template.php
+++ Configuration/TCA/Overrides/101_sys_template.php
@@ -28,61 +28,77 @@ defined('TYPO3_MODE') || die();
         'Configuration/TypoScript/Upgrade9',
         'Pizpalue - Upgrade 9'
     );
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        $_EXTKEY,
-        'Extensions/news/Configuration/TypoScript',
-        'Pizpalue - news'
-    );
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        $_EXTKEY,
-        'Extensions/eventnews/Configuration/TypoScript',
-        'Pizpalue - eventnews'
-    );
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        $_EXTKEY,
-        'Extensions/slickcarousel/Configuration/TypoScript',
-        'Pizpalue - slickcarousel'
-    );
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        $_EXTKEY,
-        'Extensions/news/Configuration/TypoScript/RSS',
-        'Pizpalue - news RSS feed'
-    );
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        $_EXTKEY,
-        'Extensions/newsslider/Configuration/TypoScript',
-        'Pizpalue - newsslider 2.0.1'
-    );
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        $_EXTKEY,
-        'Extensions/femanager/Configuration/TypoScript',
-        'Pizpalue - femanager 2.2.0'
-    );
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        $_EXTKEY,
-        'Extensions/tt_address/4.3.0/Configuration/TypoScript',
-        'Pizpalue - tt_address 4.3.0'
-    );
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        $_EXTKEY,
-        'Extensions/tt_address/GoogleMap/Configuration/TypoScript',
-        'Pizpalue - tt_address Google map'
-    );
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        $_EXTKEY,
-        'Extensions/tt_address/Teaser/Configuration/TypoScript',
-        'Pizpalue - tt_address Teaser'
-    );
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        $_EXTKEY,
-        'Extensions/indexed_search/Configuration/TypoScript',
-        'Pizpalue - indexed_search'
-    );
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        $_EXTKEY,
-        'Extensions/felogin/Configuration/TypoScript',
-        'Pizpalue - felogin'
-    );
+    if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('news')) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $_EXTKEY,
+            'Extensions/news/Configuration/TypoScript',
+            'Pizpalue - news'
+        );
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $_EXTKEY,
+            'Extensions/news/Configuration/TypoScript/RSS',
+            'Pizpalue - news RSS feed'
+        );
+    }
+    if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('eventnews')) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $_EXTKEY,
+            'Extensions/eventnews/Configuration/TypoScript',
+            'Pizpalue - eventnews'
+        );
+    }
+    if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('slickcarousel')) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $_EXTKEY,
+            'Extensions/slickcarousel/Configuration/TypoScript',
+            'Pizpalue - slickcarousel'
+        );
+    }
+    if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('newsslider')) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $_EXTKEY,
+            'Extensions/newsslider/Configuration/TypoScript',
+            'Pizpalue - newsslider 2.0.1'
+        );
+    }
+    if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('femanager')) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $_EXTKEY,
+            'Extensions/femanager/Configuration/TypoScript',
+            'Pizpalue - femanager 2.2.0'
+        );
+    }
+    if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('tt_address')) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $_EXTKEY,
+            'Extensions/tt_address/4.3.0/Configuration/TypoScript',
+            'Pizpalue - tt_address 4.3.0'
+        );
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $_EXTKEY,
+            'Extensions/tt_address/GoogleMap/Configuration/TypoScript',
+            'Pizpalue - tt_address Google map'
+        );
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $_EXTKEY,
+            'Extensions/tt_address/Teaser/Configuration/TypoScript',
+            'Pizpalue - tt_address Teaser'
+        );
+    }
+    if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('felogin')) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $_EXTKEY,
+            'Extensions/felogin/Configuration/TypoScript',
+            'Pizpalue - felogin'
+        );
+    }
+    if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('indexed_search')) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            $_EXTKEY,
+            'Extensions/indexed_search/Configuration/TypoScript',
+            'Pizpalue - indexed_search'
+        );
+    }
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
         $_EXTKEY,
         'Configuration/TypoScript/DepreciatedTheme',
@@ -92,10 +108,12 @@ defined('TYPO3_MODE') || die();
      * @deprecated since version 11.1.2, will be removed in version 12.0.0
      */
     (function ($_EXTKEY) {
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-            $_EXTKEY,
-            'Extensions/tt_address/3.0.3/Configuration/TypoScript',
-            'Pizpalue DEPRECIATED - tt_address 3.0.3'
-        );
+        if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('tt_address')) {
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+                $_EXTKEY,
+                'Extensions/tt_address/3.0.3/Configuration/TypoScript',
+                'Pizpalue DEPRECIATED - tt_address 3.0.3'
+            );
+        }
     })($_EXTKEY);
 })('pizpalue');
