c9aa9f66463b3010d194dcfcfb0d773e30214079
Roman Büchler <info@buechler.pro>
2021-12-09 17:38:25 +0200

    [RELEASE] v12.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,9 +9,9 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title'            => 'Pizpalue template',
-    'description'      => 'Extension to create websites using bootstrap. It builds upon the bootstrap_package from Benjamin Kott and increases functionality by supporting the following extensions:  container_elements, pp_gridelements, flux_elements, timelog, ws_flexslider, slickcarousel, indexed_search, news, eventnews, tt_address.',
+    'description'      => 'Extension to create websites using bootstrap. It builds upon the bootstrap_package from Benjamin Kott and increases functionality by supporting the following extensions:  container_elements, news',
     'category'         => 'template',
-    'version'          => '12.0.0-dev',
+    'version'          => '12.0.0',
     'state'            => 'stable',
     'clearCacheOnLoad' => 1,
     'author'           => 'Roman Büchler',
