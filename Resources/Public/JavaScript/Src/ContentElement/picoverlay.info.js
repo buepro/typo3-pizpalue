8b9baf15267fdab1bfc7f469986caf22df4d0990
Roman Büchler <rb@buechler.pro>
2022-11-03 08:58:33 +0100

    [TASK] Vanilize picoverlay info js

--- Resources/Public/JavaScript/Src/ContentElement/picoverlay.info.js
+++ Resources/Public/JavaScript/Src/ContentElement/picoverlay.info.js
@@ -2,19 +2,19 @@
  * Controls the overlaying from the picoverlay info type content element.
  * Together with the related css the overlay is shown when js is disabled.
  */
-(function ($) {
+(function () {
     function showOverlay () {
-        $picoverlay = $(this).closest('.pp-picoverlay').addClass('ppc-overlay');
+        this.closest('.pp-picoverlay').classList.add('ppc-overlay');
     }
     function hideOverlay () {
-        $picoverlay = $(this).closest('.pp-picoverlay').removeClass('ppc-overlay');
+        this.closest('.pp-picoverlay').classList.remove('ppc-overlay');
     }
-    let $picoverlay = $('.pp-picoverlay.ppc-info');
-    // Set flag indicating js is allowed
-    $picoverlay.addClass('ppc-init');
-    // Attach hover event handlers
-    $picoverlay.hover(showOverlay, hideOverlay);
-    // Attach click event handlers
-    $('.ppc-show', $picoverlay).on('click', showOverlay);
-    $('.ppc-hide', $picoverlay).on('click', hideOverlay);
-})(jQuery);
+    const picoverlays = document.querySelectorAll('.pp-picoverlay.ppc-info');
+    picoverlays.forEach((el) => {
+        el.classList.add('ppc-init');
+        el.addEventListener('mouseover', showOverlay);
+        el.addEventListener('mouseout', hideOverlay);
+        el.querySelector('.ppc-show').addEventListener('click', showOverlay);
+        el.querySelector('.ppc-hide').addEventListener('click', hideOverlay);
+    })
+})();
