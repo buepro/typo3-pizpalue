5e21767980c14136d1b47e671ea5f86202269866
Roman Büchler <rb@buechler.pro>
2022-11-04 09:05:51 +0100

    [TASK] Vanilize link target js

--- Resources/Public/JavaScript/Src/pizpalue.linktarget.js
+++ Resources/Public/JavaScript/Src/pizpalue.linktarget.js
@@ -2,24 +2,23 @@
  * Remove empty link targets by assigning the anchor id to the following sibling or to one of its descendents.
  */
 (function () {
-    $('.pp-link-target').each(function () {
-        let $a = $(this);
-        let id = $a.attr('id');
-        let $sibling = $a.next();
-        if ($sibling.is('a') && $sibling.text() == "") {
+    document.querySelectorAll('.pp-link-target').forEach((anchor) => {
+        let id = anchor.getAttribute('id');
+        let sibling = anchor.nextElementSibling;
+        if (sibling.matches('a') && sibling.textContent === "") {
             // The sibling was a link target too (the one for `_LOCALIZED_UID`)
-            $sibling = $sibling.next();
+            sibling = sibling.nextElementSibling;
         }
-        if (!$sibling.attr('id')) {
-            $sibling.attr('id', id);
-            $a.remove();
+        if (!sibling.getAttribute('id')) {
+            sibling.setAttribute('id', id);
+            anchor.remove();
             return;
         }
         // The sibling had an id assigned to. Let's try to assign the id to a descendent.
-        let $children = $(':not([id]), [id=""]', $sibling);
-        if ($children.length) {
-            $children.first().attr('id', id);
-            $a.remove();
+        let children = sibling.querySelectorAll(':not([id]), [id=""]');
+        if (children.length) {
+            children[0].setAttribute('id', id);
+            anchor.remove();
         }
     });
 })();
