ccbb017d461e10d56e9e262ec194bedf0ec588aa
Roman Büchler <rb@buechler.pro>
2022-11-04 09:54:04 +0100

    [TASK] Vanilize reveal footer service js

--- Resources/Public/JavaScript/Src/pizpalue.revealfooterservice.js
+++ Resources/Public/JavaScript/Src/pizpalue.revealfooterservice.js
@@ -1,4 +1,4 @@
-+function ($) {
++function () {
     /**
      * Applies an effect to the footer in a way that it is getting unrevealed while the user scrolls down hence giving
      * the impression a curtain is lifted. The effect is achieved by applying a padding to the bottom from the framing
@@ -7,82 +7,80 @@
      * The effect is disabled in case the footer content couldn't be shown entirely (e.g. when its content is using
      * more than the entire viewport height).
      */
-    var revealFooterService = {
-        $frame: null,
-        $header: null,
-        $footer: null,
+    const revealFooterService = {
+        frame: null,
+        header: null,
+        footer: null,
         // cookie consent dialog
-        $ccDialog: null,
+        ccDialog: null,
 
         get contentWindowHeight () {
-            var windowHeight = $(window).height();
+            let windowHeight = window.innerHeight;
             // reduced height if the header is fix positioned
-            if (this.$header.css('position') === 'fixed') {
-                windowHeight -= this.$header.outerHeight();
+            if (this.header.style.position === 'fixed') {
+                windowHeight -= this.header.offsetHeight;
             }
             // reduce height if cookie consent dialog is present, visible and fix positioned
-            if (this.ccDialogFixed()) {
-                windowHeight -= this.$ccDialog.outerHeight();
+            if (this.ccDialogFixed) {
+                windowHeight -= this.ccDialog.offsetHeight;
             }
             return windowHeight;
         },
 
         /**
          * Returns true in case the cookie consent dialog is present and fix positioned
-         * 
+         *
          * @return {boolean}
          */
-        ccDialogFixed: function () {
-            var dialogFillsWidth = this.$ccDialog.outerWidth(true) >= this.$frame.outerWidth(true);
-            return this.$ccDialog.length > 0
-                && !this.$ccDialog.hasClass('cc-invisible')
-                && !this.$ccDialog.hasClass('cc-static')
+        get ccDialogFixed () {
+            if (this.ccDialog === null) {
+                return false;
+            }
+            let dialogFillsWidth = pp.outerWidth(this.ccDialog) >= pp.outerWidth(this.frame);
+            return !this.ccDialog.classList.contains('cc-invisible')
+                && !this.ccDialog.classList.contains('cc-static')
                 && dialogFillsWidth;
         },
 
         init: function () {
-            this.$frame = $('.body-bg');
-            this.$header = $('.body-bg > header');
-            this.$footer = $('.body-bg > footer');
-            this.$ccDialog = $('.cc-window');
+            this.frame = document.querySelector('.body-bg');
+            this.header = document.querySelector('.body-bg > header');
+            this.footer = document.querySelector('.body-bg > footer');
+            this.ccDialog = document.querySelector('.cc-window');
         },
 
         canReveal: function () {
-            var result = this.$footer.length === 1;
-            return result && this.contentWindowHeight > this.$footer.outerHeight();
+            return this.footer !== null && this.contentWindowHeight > this.footer.offsetHeight;
         },
-        
+
         update: function () {
-            var newPadding = this.$footer.height();
+            let newPadding = this.footer.getBoundingClientRect().height;
             if (this.canReveal()) {
-                this.$frame.addClass('pp-reveal-footer');
+                this.frame.classList.add('pp-reveal-footer');
             } else {
-                this.$frame.removeClass('pp-reveal-footer');
+                this.frame.classList.remove('pp-reveal-footer');
                 newPadding = 0;
             }
             // in case the cookie consent dialog is fix positioned on the bottom the cookieconsentservice already took
             // care about offsetting the border container (footer)
-            if (this.ccDialogFixed() && this.$ccDialog.hasClass('cc-bottom')) return;
+            if (this.ccDialogFixed && this.ccDialog.classList.contains('cc-bottom')) return;
             // add padding to framing container
-            this.$frame.css('padding-bottom',newPadding);
+            this.frame.style.paddingBottom = newPadding + 'px';
         },
 
     };
 
-    $(function () {
-        // dom ready
-        $(function () {
-            revealFooterService.init();
+    pp.domReady(function () {
+        revealFooterService.init();
+        revealFooterService.update();
+        window.addEventListener('resize', function () {
             revealFooterService.update();
-            $(window).resize(function () {
-                revealFooterService.update();
-            });
-            window.addEventListener('buepro.ccs.framechange',function() {
-                revealFooterService.update();
-            });
-            window.addEventListener('buepro.ccs.borderchange',function() {
-                revealFooterService.update();
-            });
+        });
+        window.addEventListener('buepro.ccs.framechange',function() {
+            revealFooterService.update();
+        });
+        window.addEventListener('buepro.ccs.borderchange',function() {
+            revealFooterService.update();
         });
     });
-}(jQuery);
+}();
