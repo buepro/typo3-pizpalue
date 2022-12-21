2c90172e54aef4bd05e10f30af3d7ad7ca48fc64
Roman Büchler <rb@buechler.pro>
2022-12-21 19:40:28 +0100

    [BUGFIX] Access container_elements utility class

--- ext_localconf.php
+++ ext_localconf.php
@@ -183,6 +183,7 @@ defined('TYPO3') || die('Access denied.');
     if (
         (bool)($pizpalueConfiguration['autoLoadStaticTSForExtensions'] ?? true) &&
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('container_elements') &&
+        method_exists('\\Buepro\\ContainerElements\\Utility\\VersionUtility', 'getExtensionVersion') &&
         (($containerElementsVersion = Buepro\ContainerElements\Utility\VersionUtility::getExtensionVersion('container_elements'))
             === 0 || $containerElementsVersion > 3001001)
     ) {
