64cf35d80483416014ada37933213a01c9404b1a
Roman Büchler <rb@buechler.pro>
2023-01-26 09:12:35 +0100

    [!!!][TASK] Replace bootstrap scroll spy js
    Description
    ~~~~~~~~~~~
    
    Since the bootstrap scroll spy is buggy a replacement
    has been introduced, dropping the typoscript configuration
    constant `pizpalue.menu.scroll.offset` in favor of
    `pizpalue.menu.scroll.rootMargin`.
    
    Corrective action
    ~~~~~~~~~~~~~~~~~
    
    Review the typoscript scroll configuration.
    
    Related: https://github.com/twbs/bootstrap/issues/36431

--- Resources/Public/JavaScript/Src/pizpalue.scroll-navigation.js
+++ Resources/Public/JavaScript/Src/pizpalue.scroll-navigation.js
@@ -1,29 +1,41 @@
-(function () {
+/**
+ * @see https://yashmahalwal.medium.com/scrollspy-using-intersection-observer-36acb7520e46
+ */
+pp.domReady(function () {
     const data = JSON.parse(document.getElementById('pp-scrollnav-data').dataset.settings);
     const menu = document.getElementById(data.menuId);
-    if (!menu) {
-        return;
-    }
+    const menuItems = menu.querySelectorAll('.nav-item');
+    const links = [...menu.getElementsByTagName('a')];
+
     document.body.setAttribute('tabindex', '0');
     document.body.classList.add('pp-has-scrollnav');
-    window.addEventListener('activate.bs.scrollspy', (e) => {
-        const a = menu.querySelector('[href="' +  e.relatedTarget + '"]');
-        if (!a) {
-            return;
-        }
-        menu.querySelectorAll('.nav-item').forEach((el) => {
-            el.classList.remove('active');
-            if (!el.querySelector('[href="' +  e.relatedTarget + '"]')) {
-                el.getElementsByTagName('a')[0].blur();
+
+    const observer = new IntersectionObserver(
+        (entries) => {
+            for (const entry of entries) {
+                if (entry.isIntersecting) {
+                    // Activate the link associated with the section
+                    menuItems.forEach((menuItem) => {
+                        menuItem.classList.remove('active');
+                    });
+                    const id = entry.target.getAttribute('id');
+                    const link = document.querySelector('[href="#' + id + '"]');
+                    const menuItem = link.closest('.nav-item') ?? link.parentElement;
+                    menuItem.classList.add('active');
+                }
             }
-        });
-        const item = a.closest('.nav-item') ?? a.parentElement;
-        if (item) {
-            item.classList.add('active');
+        },
+        {
+            rootMargin: data.rootMargin,
         }
+    );
+
+    links.forEach((link) => {
+        // Add section to observer
+        observer.observe(document.getElementById(link.href.split('#').pop()));
+        // Remove focus from clicked link
+        link.addEventListener('click', (e) => {
+            e.target.blur();
+        })
     });
-    new bootstrap.ScrollSpy(document.body, {
-        target: '#' + data.menuId,
-        offset: parseInt(data.offset)
-    });
-})();
+});
