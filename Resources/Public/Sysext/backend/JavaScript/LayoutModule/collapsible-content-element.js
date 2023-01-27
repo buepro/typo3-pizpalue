8ee28254bf2fed421c2a534153502b9fc05a5f5c
Roman Büchler <rb@buechler.pro>
2023-01-27 09:33:13 +0100

    [TASK] Correct template for collapsible ce's in be

--- Resources/Public/Sysext/backend/JavaScript/LayoutModule/collapsible-content-element.js
+++ Resources/Public/Sysext/backend/JavaScript/LayoutModule/collapsible-content-element.js
@@ -1,10 +1,16 @@
 (function () {
-    function getTitleElement(collapsible) {
-        return collapsible.closest('.pp-page-ce').querySelector('.ppc-title');
+    function getTitleElement(collapsible, selector) {
+        return collapsible.closest('.pp-page-ce').querySelector('.' + selector);
     }
     const pageCeCollapsibles = document.querySelectorAll('.pp-page-ce .ppc-collapse');
     pageCeCollapsibles.forEach((el) => {
-        el.addEventListener('show.bs.collapse', (e) => getTitleElement(e.target).classList.add('d-none'));
-        el.addEventListener('hide.bs.collapse', (e) => getTitleElement(e.target).classList.remove('d-none'));
+        el.addEventListener('show.bs.collapse', (e) => {
+            getTitleElement(e.target, 'ppc-expanded').classList.remove('d-none');
+            getTitleElement(e.target, 'ppc-collapsed').classList.add('d-none');
+        });
+        el.addEventListener('hide.bs.collapse', (e) => {
+            getTitleElement(e.target, 'ppc-expanded').classList.add('d-none');
+            getTitleElement(e.target, 'ppc-collapsed').classList.remove('d-none');
+        });
     })
 })();
