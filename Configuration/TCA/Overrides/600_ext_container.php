1047a7c732fc7b32dc2092277c939756d04a7752
Roman Büchler <rb@buechler.pro>
2023-02-27 16:36:42 +0200

    [BUGFIX] Use deprecated backend partials

--- Configuration/TCA/Overrides/600_ext_container.php
+++ Configuration/TCA/Overrides/600_ext_container.php
@@ -7,16 +7,24 @@
  * LICENSE file that was distributed with this source code.
  */
 
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 defined('TYPO3') or die('Access denied.');
 
 (static function (): void {
     /**
-     * @todo Might be removed upon solving b13/container issue 272
+     * @link https://github.com/b13/container/issues/272
      */
-    if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('container')) {
+    if (ExtensionManagementUtility::isLoaded('container')) {
+        $backendPartialsPath = 'EXT:pizpalue/Sysext/backend/Resources/Private/Partials';
+        if (GeneralUtility::makeInstance(Typo3Version::class)->getMajorVersion() === 11) {
+            $backendPartialsPath = 'EXT:pizpalue/Sysext/backend/Resources/Deprecated/Private/Partials';
+        }
         foreach ($GLOBALS['TCA']['tt_content']['containerConfiguration'] as &$config) {
-            if (!isset(array_flip($config['gridPartialPaths'])['EXT:pizpalue/Sysext/backend/Resources/Private/Partials'])) {
-                $config['gridPartialPaths'][] = 'EXT:pizpalue/Sysext/backend/Resources/Private/Partials';
+            if (!isset(array_flip($config['gridPartialPaths'])[$backendPartialsPath])) {
+                $config['gridPartialPaths'][] = $backendPartialsPath;
             }
         }
         unset($config);
