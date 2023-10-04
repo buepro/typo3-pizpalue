3bc2698d100b23d648687aad33e8f69d6f591829
Roman Büchler <rb@buechler.pro>
2023-10-04 11:50:01 +0300

    [TASK] Optimize variable scope

--- Resources/Public/JavaScript/Src/pizpalue.utility.js
+++ Resources/Public/JavaScript/Src/pizpalue.utility.js
@@ -16,8 +16,8 @@ if ( typeof pp !== 'undefined' ) {
              */
             getUrlParameter: function (name) {
                 name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-                var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-                var results = regex.exec(location.search);
+                const regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+                const results = regex.exec(location.search);
                 return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
             },
 
