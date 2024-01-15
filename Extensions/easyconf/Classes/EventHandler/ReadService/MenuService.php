77a1ffde3e4b5e3ddb4d2fc2905ea1709d1ab650
Roman Büchler <rb@buechler.pro>
2024-01-15 12:31:04 +0200

    [FEATURE] Use typolink for fast menu page link
    This allows to define as well file, external, tel
    and mailto links. Additionally, a title tag can
    be defined.

--- Extensions/easyconf/Classes/EventHandler/ReadService/MenuService.php
+++ Extensions/easyconf/Classes/EventHandler/ReadService/MenuService.php
@@ -27,12 +27,6 @@ class MenuService extends AbstractService
                 $this->formFields[$field] = 'tt_content_' . $this->formFields[$field];
             }
         }
-        foreach (['menu_fast_items_first_page_uid', 'menu_fast_items_second_page_uid',
-                     'menu_fast_items_third_page_uid'] as $field) {
-            if (isset($this->formFields[$field]) && (int)$this->formFields[$field] > 0) {
-                $this->formFields[$field] = 'pages_' . $this->formFields[$field];
-            }
-        }
         return $this;
     }
 
