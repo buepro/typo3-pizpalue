93816f04ed0ef01f9b42de94a2553786265c2c51
Roman Büchler <rb@buechler.pro>
2024-03-31 12:41:20 +0300

    [BUGFIX] Enable news dummy image and variants

--- Configuration/TCA/Overrides/600_ext_news.php
+++ Configuration/TCA/Overrides/600_ext_news.php
@@ -12,6 +12,38 @@ defined('TYPO3') or die('Access denied.');
 (static function (): void {
     if (
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('news') &&
+        \Buepro\Pizpalue\Utility\VersionUtility::getExtensionVersion('news') >= 11000000
+    ) {
+        $typeList = [];
+        foreach ($GLOBALS['TCA']['tt_content']['types'] as $key => &$type) {
+            if (strpos((string)$key, 'news_') !== 0) {
+                continue;
+            }
+            $typeList[] = $key;
+            // Configure dummy asset field
+            $type['columnsOverrides']['assets']['label'] =
+                'LLL:EXT:pizpalue/Resources/Private/Language/locallang_db.xlf:tx_pizpalue_ttc.dummyAsset';
+            $type['columnsOverrides']['assets']['config']['maxitems'] = 1;
+            $type['columnsOverrides']['assets']['config']['overrideChildTca']['columns']['crop']['config']
+                ['cropVariants'] = \Buepro\Pizpalue\Utility\TcaUtility::getCropVariants();
+        }
+        unset($type);
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+            'tt_content',
+            'assets',
+            implode(',', $typeList),
+            'after:pi_flexform'
+        );
+        // Set cropping for media in news records
+        $GLOBALS['TCA']['tx_news_domain_model_news']['columns']['fal_media']['config']['overrideChildTca']['columns']
+            ['crop']['config']['cropVariants'] = \Buepro\Pizpalue\Utility\TcaUtility::getCropVariants();
+    }
+
+    // Legacy version
+    // @todo Mark deprecated
+    if (
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('news') &&
+        \Buepro\Pizpalue\Utility\VersionUtility::getExtensionVersion('news') < 11000000 &&
         // The following prevents errors in case news is installed after pizpalue
         isset($GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['news_pi1'])
     ) {
